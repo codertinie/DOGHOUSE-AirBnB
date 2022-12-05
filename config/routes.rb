4b317a3227a842f20376e14f17419ab78680dfdb
nziokikevin <kevinnzioki10@gmail.com>
2022-12-05 15:51:13 +0300

    authorization and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :dog_houses
-  resources :users
+  
+  get "/me", to: "users#show"
+
+  post "/signup", to: "users#create"
+
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
