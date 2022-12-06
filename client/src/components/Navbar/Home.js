5bdde67b53f20d5d759c6321de87f421fb3d55bb
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-12-06 11:21:42 +0300

    created navbar and about us page

--- client/src/components/Navbar/Home.js
+++ client/src/components/Navbar/Home.js
@@ -2,21 +2,18 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import "../styles/Home.css";
 import Navbar from "./Navbar";
+// import Single from "../Single";
 
-
-import Single from "../Single";
 function Home() {
     const navigate = useNavigate()
     function handleClick() {
-        navigate("/")
+        navigate("/single")
     }
     return (
         <>
             <Navbar />
             <div className="home" style={{ backgroundImage: 'logo192.jpg' }}>
                 <div className="headerContainer">
-
-                    <h1> DOGHOUSES-AIRBNB </h1>
                     <Link to="/single">
                         <button onClick={handleClick}>View More</button>
                     </Link>
