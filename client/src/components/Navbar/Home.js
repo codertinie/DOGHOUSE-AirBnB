990b94d52fc54135d6887a2b0df0d7c39ac022ab
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-12-06 10:08:40 +0300

    Added routing

--- client/src/components/Navbar/Home.js
+++ client/src/components/Navbar/Home.js
@@ -1,25 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "../styles/Home.css";
+import { Link, useNavigate } from "react-router-dom";
+// import "../styles/Home.css";
 import Navbar from "./Navbar";
 
+
+import Single from "../Single";
 function Home() {
-  return (
-    <>
-    <Navbar />
-    <div className="home" style={{ backgroundImage: 'logo192.jpg' }}>
-      <div className="headerContainer">
-        
-        <h1> COMMS Pizzeria </h1>
-        <Link to="/menu">
-          <button> ORDER NOW </button>
-        </Link>
-      </div>
-    </div>
-   
-    </>
-    
-  );
+    const navigate = useNavigate()
+    function handleClick() {
+        navigate("/")
+    }
+    return (
+        <>
+            <Navbar />
+            <div className="home" style={{ backgroundImage: 'logo192.jpg' }}>
+                <div className="headerContainer">
+
+                    <h1> DOGHOUSES-AIRBNB </h1>
+                    <Link to="/single">
+                        <button onClick={handleClick}>View More</button>
+                    </Link>
+                </div>
+            </div>
+
+        </>
+
+    );
 }
 
 export default Home;
