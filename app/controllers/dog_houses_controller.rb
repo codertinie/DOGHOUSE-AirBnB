2b4d88563a534f62bff8ff71ac52cc34ab372721
codertinie <martin.munyao@moringaschool.com>
2022-12-06 11:03:01 +0300

    Added show action and error rescuing in application controller

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -3,4 +3,13 @@ class DogHousesController < ApplicationController
   def index
     render json: DogHouse.all
   end
+
+  def show
+    doghouse = DogHouse.find_by(id: params[:id])
+    if doghouse
+      render json: doghouse
+    else
+      render json: {error: "DogHouse not found"}, status: :not_found
+    end
+  end
 end
