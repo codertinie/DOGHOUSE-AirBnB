888799a84b1b357c25f83b638481e6219ca2f6e8
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-12-08 12:44:02 +0300

    finished styling home component

--- client/src/components/Navbar/Home.js
+++ client/src/components/Navbar/Home.js
@@ -1,26 +1,29 @@
-import React,{useEffect, useState} from "react";
-import { Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../../styles/Home.css";
 import Navbar from "./Navbar";
-import Single from '../Single'
+// import Single from '../Single'
 // import Single from "../Single";
 
-function Home({dogHouses, click}) {
+function Home({ dogHouses, click }) {
 
 
     const dogHouse = dogHouses.map((house) => (
         <>
-            <div className="card">
-            <img src={house.image_url} alt=""/>
-            <h3>{house.name}</h3>
-            <h3>Location: {house.location}</h3>
-            <p><span>Price:</span>{house.price}</p>
-                    <Link to="/single">
-                        <button onClick={()=>click(house.id)} >View More</button>
-                    </Link>
-
-        </div>
-        </> ))
+            <div className="card" key={house.id}>
+                <img src={house.image_url} alt={house.image_url} />
+                <div className="centered">
+                    <h3>{house.name}</h3>
+                    <h3>Location: {house.location}</h3>
+                    <p><span>Price:</span>{house.price}</p>
+                </div>
+                <br></br>
+                <br></br>
+                <Link to="/single">
+                        <button onClick={() => click(house.id)} >View More</button>
+                </Link>
+            </div>
+        </>))
     // const navigate = useNavigate()
     // function handleClick() {
     //     navigate("/single")
@@ -28,7 +31,7 @@ function Home({dogHouses, click}) {
     return (
         <>
             <Navbar />
-            <div class="container">
+            <div className="container">
                 {dogHouse}
             </div>
 
