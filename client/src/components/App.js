6e04c9500bf463b222918171187ff2424f6ed046
iSaacSigei <kiplangatisack.is@gmail.com>
2022-12-08 21:40:25 +0300

    update app js to allow specific user to post a review

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,10 @@ import Single from './Single';
 import Navbar from './Navbar/Navbar';
 function App() {
   const [user, setUser] = useState(null)
+  const [dogHouses, setDogHouses] = useState([])
+  const [house, setHouse] = useState({})
+  const [reviews, setReviews]=useState(false)
+
   useEffect(() => {
     fetch('/me')
       .then((r) => {
@@ -16,8 +20,7 @@ function App() {
         }
       })
   }, [])
-  const [dogHouses, setDogHouses] = useState([])
-  const [house, setHouse] = useState({})
+
   useEffect(() => {
     fetch('/dog_houses')
       .then(response => response.json())
@@ -36,13 +39,16 @@ function App() {
         setHouse(data)
       })
   }
+  function fetchPostedReview(){
+    setReviews(reviews=>!reviews)
+  }
   //console.log(house)
   return (
     <BrowserRouter>
       <Navbar user={user} setUser={setUser}/>
       <Routes>
         <Route exact path='/' element={<Home dogHouses={dogHouses} click={click} user={user} />} />      
-        <Route path={`/dog_houses/${house.id}`} element={<Single house={house} />} />
+        <Route path={`/dog_houses/${house.id}`} element={<Single fetchPostedReview={fetchPostedReview} house={house} user={user} />} />
         <Route path='/about' element={<About />} />
         <Route path='/login' element={<Login onLogin={setUser} />} />
         <Route path='/signup' element={<Signup onLogin={setUser} />} />
